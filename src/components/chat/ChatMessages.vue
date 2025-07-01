e8f0f4c3bcdd82a2891cdee8f3f7c00fb02cfc23
V1laZ <pasikstudiocz@gmail.com>
2025-07-01 20:39:12 +0200

    feat: show robot icon avatar for BanchoBot

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -22,12 +22,24 @@
         <div class="flex items-start space-x-3">
           <!-- Avatar -->
           <div class="flex-shrink-0 mt-1">
-            <div class="w-8 h-8 bg-gradient-to-br from-pink-500 to-purple-600 rounded-full flex items-center justify-center">
+            <div 
+              class="size-8 rounded-full flex items-center justify-center"
+              :class="{
+                'bg-gray-600': message.username === 'BanchoBot',
+                'bg-gradient-to-br from-pink-500 to-purple-600': message.username !== 'BanchoBot'
+              }"
+            >
               <span class="text-sm font-medium text-white">
-                {{ message.username.charAt(0).toUpperCase() }}
+                <template v-if="message.username === 'BanchoBot'">
+                  <svg  class="size-5 -mt-0.5" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M22 14h-1c0-3.87-3.13-7-7-7h-1V5.73A2 2 0 1 0 10 4c0 .74.4 1.39 1 1.73V7h-1c-3.87 0-7 3.13-7 7H2c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1h1v1a2 2 0 0 0 2 2h14c1.11 0 2-.89 2-2v-1h1c.55 0 1-.45 1-1v-3c0-.55-.45-1-1-1M8.68 17.04L7.5 15.86l-1.18 1.18l-1.18-1.18L7.5 13.5l2.36 2.36zm9 0l-1.18-1.18l-1.18 1.18l-1.18-1.18l2.36-2.36l2.36 2.36z"/></svg>
+                </template>
+                <template v-else>
+                  {{ message.username.charAt(0).toUpperCase() }}
+                </template>
               </span>
             </div>
           </div>
+            
 
           <!-- Message Content -->
           <div class="flex-1 min-w-0">
