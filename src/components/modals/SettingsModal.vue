8290bcf91e1d3ae611e0862d1fcdf0bd0e2ee026
V1laZ <pasikstudiocz@gmail.com>
2025-12-14 15:39:52 +0100

    fix: remove space

--- src/components/modals/SettingsModal.vue
+++ src/components/modals/SettingsModal.vue
@@ -118,7 +118,7 @@
       <!-- Footer -->
       <div class="mt-8 pt-6 border-t border-gray-700">
         <div class="flex items-center justify-between text-sm text-gray-400">
-          <span>osu! Reffer v{{ appVersion }}</span>
+          <span>osu!Reffer v{{ appVersion }}</span>
         </div>
       </div>
     </div>
